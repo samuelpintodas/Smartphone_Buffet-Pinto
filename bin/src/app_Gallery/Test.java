04f575834bfd832400bcb947f12da16d6e3480c0
samuelpintodas <samuel.pintodasilva@students.hevs.ch>
2018-05-23 13:51:08 +0200

    test commit to master

---
+++ bin/src/app_Gallery/Test.java
@@ -0,0 +1,14 @@
+// Classe de test de l'app gallerie
+
+
+package app_Gallery;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int t = 2;
+	}
+
+}
